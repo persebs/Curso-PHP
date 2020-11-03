03e904347699c06456bf4d2bb996b65eb6fde1f1
Rafael Lopes da Costa <40012161+persebs@users.noreply.github.com>
2020-11-03 18:37:28 -0300

    Unidade 6
    Concluida a unidade 6

---
+++ unidade06/inicial.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP FUNDAMENTAL</title>
+    </head>
+
+    <body>
+    <!--COMENTÁRIO DE HTML-->
+        <p><?php 
+            //COMENTÁRIO EM PHP
+            $_1nome = "Rafael Lopes da Costa"; 
+            echo $_1nome
+        ?></p>
+        <p> <?php echo 5*4 ?></p>
+    </body>
+</html>
