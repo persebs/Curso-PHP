a7273227a24ede8ab9cfe90b44d82aad169f85a4
Rafael Lopes da Costa <rafael.l.costa@outlook.com>
2020-12-14 21:42:09 -0300

    Continuação

--- unidade13/destino.php
+++ unidade13/destino.php
@@ -3,8 +3,21 @@
     <head>
         <meta charset="UTF-8">
         <title>Curso PHP FUNDAMENTAL</title>
+
+        <style>
+            img { border:1px solid #ccc }
+        </style>
     </head>
 
-    <body> 
+    <body>
+        
+        <?php
+
+            $_salada = array("imagens/laranja.jpg","imagens/maca.jpg","imagens/abacate.jpg");
+            $_codigo = $_GET["codigo"];
+        ?>
+
+        <img src="<?php echo $_salada[$_codigo]?>">
+
     </body>
 </html>
--- unidade14/destino.php
+++ unidade14/destino.php
@@ -5,6 +5,43 @@
         <title>Curso PHP FUNDAMENTAL</title>
     </head>
 
-    <body> 
+    <body>
+
+    <?php
+        if (isset($_POST["nome"])){
+
+        $_nome = $_POST["nome"];
+
+        }else{
+        $_nome = "Sem definição";
+
+        }
+
+        if (isset($_POST["email"])){
+
+            $_email = $_POST["email"];
+    
+            }else{
+            $_email = "Sem definição";
+    
+            }
+            
+    echo "nome: " . $_nome . "<br>";
+    echo "email: " . $_email . "<br><br>";
+
+
+    // Com operador ternário
+    
+
+
+    $_nome2 = isset($_POST["nome"]) ? $_POST["nome"] : "Sem definição";
+    $_email2 = isset($_POST["email"]) ? $_POST["email"] : "Sem definição";
+
+    echo "nome ternário: " . $_nome2 . "<br>";
+    echo "email ternario: " . $_email2 . "<br>";
+
+
+    ?>
+
     </body>
 </html>
